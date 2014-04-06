2cc19de07463db0aacde4fc3038973e7defb6742
Daniil Baturin <daniil@baturin.org>
2014-04-06 13:50:42 +0700

    Add check for nosetests to the test runner script.

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-PYTHONPATH=libraries nosetests -w tests/unit/
+which nosetests 2>&1 >/dev/null
+if [ $? != 0 ]; then
+    echo "nosetests binary not found"
+    echo "Install nose (https://pypi.python.org/pypi/nose/‎) or make sure it is in your PATH"
+    exit 1
+fi
+
+PYTHONPATH=libraries:tests/unit nosetests -w tests/unit/
