500e5e05cd95769713af68000a28ac1864b33f8b
Daniil Baturin <daniil@baturin.org>
2014-04-18 10:26:49 +0700

    Make all vytree.Node test independent.

--- tests/unit/node_test.py
+++ tests/unit/node_test.py
@@ -25,60 +25,67 @@ import unittest
 
 class TestVytreeNode(unittest.TestCase):
 
-    def setUp(self):
-        self.node = vytree.Node("root")
-
     def test_create_node(self):
-        self.assertIsInstance(self.node, vytree.Node)
+        node = vytree.Node("root")
+        self.assertIsInstance(node, vytree.Node)
 
     def test_get_name(self):
-        self.assertEqual(self.node.get_name(), "root")
+        node = vytree.Node("root")
+        self.assertEqual(node.get_name(), "root")
 
     def test_insert_immediate_child(self):
-        child = self.node.insert_child(['foo'])
+        node = vytree.Node("root")
+        child = node.insert_child(['foo'])
         self.assertIsInstance(child, vytree.Node)
 
     def test_insert_duplicate_child(self):
-        self.node.insert_child(['foo'])
+        node = vytree.Node("root")
+        node.insert_child(['foo'])
         self.assertRaises(vytree.ChildAlreadyExistsError,
-                          self.node.insert_child,
+                          node.insert_child,
                           ['foo'])
 
     def test_find_immediate_child(self):
-        self.node.insert_child(['foo'])
-        child = self.node.find_child('foo')
+        node = vytree.Node("root")
+        node.insert_child(['foo'])
+        child = node.find_child('foo')
         self.assertIsInstance(child, vytree.Node)
 
     def test_get_child(self):
-        child = self.node.insert_child(['foo'])
+        node = vytree.Node("root")
+        child = node.insert_child(['foo'])
         self.assertIsInstance(child, vytree.Node)
 
     def test_get_child_multi_level(self):
-        foo_child = self.node.insert_child(['foo'])
+        node = vytree.Node("root")
+        foo_child = node.insert_child(['foo'])
         bar_child = foo_child.insert_child(['bar'])
-        self.assertEqual(bar_child, self.node.get_child(['foo', 'bar']))
+        self.assertEqual(bar_child, node.get_child(['foo', 'bar']))
 
     def test_delete_child(self):
-        self.node.insert_child(['foo'])
-        self.node.delete_child(['foo'])
+        node = vytree.Node("root")
+        node.insert_child(['foo'])
+        node.delete_child(['foo'])
         self.assertRaises(vytree.ChildNotFoundError,
-                          self.node.find_child,
+                          node.find_child,
                           ['foo'])
 
     def test_delete_child_multi_level(self):
-        self.node.insert_child(['foo', 'bar'])
-        self.node.delete_child(['foo', 'bar'])
+        node = vytree.Node("root")
+        node.insert_child(['foo', 'bar'])
+        node.delete_child(['foo', 'bar'])
         self.assertRaises(vytree.ChildNotFoundError,
-                          self.node.find_child,
+                          node.find_child,
                           ['foo', 'bar'])
-        self.assertIsInstance(self.node.get_child(['foo']),
+        self.assertIsInstance(node.get_child(['foo']),
                               vytree.Node)
 
     def test_delete_child_subtree(self):
-        self.node.insert_child(['foo', 'bar'])
-        self.node.delete_child(['foo'])
+        node = vytree.Node("root")
+        node.insert_child(['foo', 'bar'])
+        node.delete_child(['foo'])
         self.assertRaises(vytree.ChildNotFoundError,
-                          self.node.find_child,
+                          node.find_child,
                           ['foo', 'bar'])
 
     def test_exists(self):
