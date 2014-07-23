97b7aa158b64e89aa36c0a2e5864a9db769b9771
Daniil Baturin <daniil@baturin.org>
2014-07-24 00:03:49 +0700

    Some sh flavours don't like ==.

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -18,7 +18,7 @@
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 #    USA
 
-if [ "$NOSETESTS" == "" ]; then
+if [ "$NOSETESTS" = "" ]; then
     NOSETESTS=nosetests
 fi
 echo "Using $NOSETESTS for nosetests binary"
