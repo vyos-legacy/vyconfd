03388f59cd864e25a5aaf92c7f28f1c54495158d
Endre Karlson <endre.karlson@hp.com>
2014-08-26 20:17:53 +0200

    Make installation and setup easier.
    This adds 'pbr' which provides some reasonable things in terms of versioning
    etc to python projects. See https://pypi.python.org/pypi/pbr/ for info.
    
    Also specify requirements in *requirements.txt files.

---
+++ setup.py
@@ -0,0 +1,34 @@
+# vyconf.types: base classes for VyConf node value validation
+#
+# Copyright (C) 2014 VyOS Development Group <maintainers@vyos.net>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+# USA
+
+# NOTE: To add new requirements change / modify *requirements.txt files.
+
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
