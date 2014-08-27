ec16a985f7eb64c3abb06e34e2f8d773d6ec5e60
Endre Karlson <endre.karlson@hp.com>
2014-08-27 10:03:34 +0200

    Make rnc2rng more CI friendly
    The script now becomes useful as: SCEMAS_PATH=<path> scripts/rnc2rng.sh to
    compile all schemas in SCHEMAS_PATH or you can compile one schema with
    scipts/rnc2rng.sh <src> <dst>

--- scripts/rnc2rng.sh
+++ scripts/rnc2rng.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 which trang 2>&1 >/dev/null
 if [ $? != 0 ]; then
@@ -7,17 +7,50 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-source=$1
-destination=$2
-if [ ! -f $source ]; then
-    echo "Source file $source does not exist"
-    exit 1
-fi
 
-if [ -z $destination ]; then
-    echo "Please specify destination file"
-    exit 1
-fi
+function compile_one() {
+    local src=$1
+    local dst=$2
+    if [ ! -f "$src" ]; then
+        echo "Source file $src does not exist"
+        exit 1
+    fi
+    if [ -z "$dst" ]; then
+        echo "Please specify destination file"
+        exit 1
+    fi
 
-echo "Writing RelaxNG into $destination"
-trang -Irnc -Orng $source $destination
+    echo "Writing RelaxNG from $src to $dst"
+    trang -Irnc -Orng $src $dst
+}
+
+# Loop over all rnc files in a directory compiling them to rnc with the same name with different ending
+function compile_all_in_path() {
+    local path=$1
+    if [ ! -d "$path"  ]; then
+        echo "Directory $path was not found"
+        exit 1
+    fi
+
+    schemas=$(find $path -type f -name '*.rnc')
+
+    for schema in $schemas; do
+        compile_one $schema $(echo $schema | sed 's/\.rnc$/.rng/g')
+    done
+}
+
+case $1 in
+   all)
+        path=${SCHEMAS_PATH:-$2}
+        echo "Compiling all rnc schemas in $path"
+        compile_all_in_path "$path"
+    ;;
+    one)
+        compile_one $2 $3
+    ;;
+    *)
+        echo "Usage:"
+        echo "$0 all <path>: compiles rnc all schemas in <path> to rng"
+        echo "$0 one <source> <destination>: compiles rnc <source> to rng <destination>"
+    ;;
+esac
